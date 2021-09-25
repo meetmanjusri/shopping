2fcede52472346c3a2a69dcf428df64adf45150e
meetmanjusri <meetmanjusri@gmail.com>
2021-09-24 21:43:39 -0500

    Created models and admins

--- maplegrocery/admin.py
+++ maplegrocery/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class UnitAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description', 'created_date', 'updated_date']
+    list_filter = ['name', 'description', 'created_date', 'updated_date']
+    prepopulated_fields = {'description': ('name',)}
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'created_date', 'updated_date']
+    list_filter = ['name', 'created_date', 'updated_date']
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description', 'image', 'price', 'available', 'product_unit', 'product_category',
+                    'created_date', 'updated_date']
+    list_filter = ['name', 'description', 'image', 'price', 'available', 'product_unit', 'product_category',
+                   'created_date', 'updated_date']
+
+
+class ReceiptAdmin(admin.ModelAdmin):
+    list_display = ['payment_card', 'total_price', 'created_date', 'updated_date']
+    list_filter = ['payment_card', 'total_price', 'created_date', 'updated_date']
+
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ['item_receipt', 'item_product', 'item_product_price', 'created_date', 'updated_date']
+    list_filter = ['item_receipt', 'item_product', 'item_product_price', 'created_date', 'updated_date']
+
+
+admin.site.register(Unit, UnitAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Receipt, ReceiptAdmin)
+admin.site.register(Item, ItemAdmin)
--- maplegrocery/models.py
+++ maplegrocery/models.py
@@ -1,3 +1,97 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Unit(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField(max_length=280, null=True, blank=True)
+    created_date = models.DateTimeField(default=timezone.now)
+    updated_date = models.DateTimeField(auto_now_add=True)
+
+    def created(self):
+        self.created_date = timezone.now()
+        self.save()
+
+    def updated(self):
+        self.updated_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    created_date = models.DateTimeField(default=timezone.now)
+    updated_date = models.DateTimeField(auto_now_add=True)
+
+    def created(self):
+        self.created_date = timezone.now()
+        self.save()
+
+    def updated(self):
+        self.updated_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField(max_length=280, null=True, blank=True)
+    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    available = models.BooleanField(default=True)
+    product_unit = models.ForeignKey(Unit, on_delete=models.CASCADE)
+    product_category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(default=timezone.now)
+    updated_date = models.DateTimeField(auto_now_add=True)
+
+    def created(self):
+        self.created_date = timezone.now()
+        self.save()
+
+    def updated(self):
+        self.updated_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Receipt(models.Model):
+    payment_card = models.CharField(max_length=16)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    created_date = models.DateTimeField(default=timezone.now)
+    updated_date = models.DateTimeField(auto_now_add=True)
+
+    def created(self):
+        self.created_date = timezone.now()
+        self.save()
+
+    def updated(self):
+        self.updated_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return f'{self.id}'
+
+
+class Item(models.Model):
+    item_receipt = models.ForeignKey(Receipt, on_delete=models.CASCADE)
+    item_product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    item_product_price = models.DecimalField(max_digits=10, decimal_places=2)
+    created_date = models.DateTimeField(default=timezone.now)
+    updated_date = models.DateTimeField(auto_now_add=True)
+
+    def created(self):
+        self.created_date = timezone.now()
+        self.save()
+
+    def updated(self):
+        self.updated_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return f'{self.id}'
