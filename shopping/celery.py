5d9a23de39fd6f6b07d942277870af4def3d81ed
meetmanjusri <meetmanjusri@gmail.com>
2021-11-14 00:02:39 -0600

    Removed Celery

--- shopping/celery.py
+++
@@ -1,9 +0,0 @@
-import os
-from celery import Celery
-
-# set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shopping.settings')
-
-app = Celery('shopping')
-app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks()
