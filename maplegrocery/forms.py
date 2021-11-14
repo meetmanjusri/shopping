9dbbfd314ffe4780d748d82567fdbd70863c15a2
Manju Muthukumaran <meetmanjusri@gmail.com>
2021-11-14 12:33:16 -0600

    Updated calendar picker for customer preferred time

--- maplegrocery/forms.py
+++ maplegrocery/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import Unit, Product, Category, Order, OrderItem
+from django.forms.widgets import DateInput
 
 
 class UnitForm(forms.ModelForm):
@@ -23,7 +24,11 @@ class ProductForm(forms.ModelForm):
 class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
-        fields = ('first_name', 'last_name', 'email', 'address', 'postal_code', 'city')
+        fields = ('first_name', 'last_name', 'email', 'address', 'postal_code', 'city', 'preferred_date', 'preferred_time')
+        # Reference: https: // stackoverflow.com / questions / 3367091 / whats - the - cleanest - simplest - to - get - running - datepicker - in -django
+        # https: // stackoverflow.com / questions / 55404397 / how - to - use - timeinput - widget - in -django - forms
+        widgets = {'preferred_date': DateInput(attrs={'type': 'date'}),
+                   'preferred_time': forms.TimeInput(attrs={'type': 'time'})}
 
 
 class OrderItemForm(forms.ModelForm):
--- maplegrocery/models.py
+++ maplegrocery/models.py
@@ -75,6 +75,8 @@ class Order(models.Model):
     postal_code = models.CharField(max_length=20)
     city = models.CharField(max_length=100)
     braintree_id = models.CharField(max_length=150, blank=True)
+    preferred_date = models.DateField('Date')
+    preferred_time = models.TimeField('Time')
     created_date = models.DateTimeField(default=timezone.now)
     updated_date = models.DateTimeField(auto_now_add=True)
     paid = models.BooleanField(default=False)
