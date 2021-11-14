0a41fa9db6522abbe2a526b4eeb9e205c5789c50
meetmanjusri <meetmanjusri@gmail.com>
2021-11-14 08:59:11 -0600

    Updated the base container

--- shopping/settings.py
+++ shopping/settings.py
@@ -152,7 +152,7 @@ AWS_DEFAULT_ACL = None
 AWS_LOCATION = 'static'
 
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-DEFAULT_FILE_STORAGE = 'shopping.storage_backends.MediaStorage'
+# DEFAULT_FILE_STORAGE = 'shopping.storage_backends.MediaStorage'
 STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 
 # Static files (CSS, JavaScript, Images)
